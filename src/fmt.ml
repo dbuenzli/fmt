e9065fa38e58f66a9eab9dc4d2a2c8566247a69c
Daniel Bünzli <daniel.buenzli@erratique.ch>
2021-10-03 23:33:08 +0200

    Remove some watermarks.

--- src/fmt.ml
+++ src/fmt.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2014 The fmt programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 let invalid_arg' = invalid_arg
